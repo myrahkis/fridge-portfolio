6e13b81d24bcbc21e4b818e0b8599b07fc9da8df
Mira <kruzinka@yandex.ru>
2024-12-06 21:03:23 +0300

    styled tooltips

--- src/features/magnets/MagnetsTooltips.jsx
+++ src/features/magnets/MagnetsTooltips.jsx
@@ -1,21 +1,53 @@
 import { Tooltip } from "react-tooltip";
 import Modal from "../../ui/Modal";
 import MagnetInfo from "./MagnetInfo";
+import styled from "styled-components";
 
 const linkGithub = "https://github.com/myrahkis";
 const linkMeme = "https://yandex.ru/video/preview/14730867519621346732";
 
+const Container = styled.div`
+  .custom-tooltip {
+    backdrop-filter: blur(8px);
+    border-radius: 50%;
+    padding: 2rem 2.5rem;
+    background-color: #258074;
+  }
+`;
+
+const Name = styled.p`
+  cursor: pointer;
+  color: white;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+
+  &:link,
+  &:visited {
+    color: white;
+  }
+
+  &:active {
+    color: #6aaea3;
+  }
+`;
+
 function MagnetsTooltips() {
   return (
-    <>
+    <Container>
+      {/* modals */}
       <Modal>
         <Tooltip
+          className="custom-tooltip"
           id="magnet1"
           content={
             <Modal.Open opens="credits">
-              <p>Credits</p>
+              <Name>Credits</Name>
             </Modal.Open>
           }
+          arrowColor="#00584d"
+          border="3px solid #00584d"
           clickable
         />
         <Modal.Window name="credits" width="34">
@@ -25,12 +57,15 @@ function MagnetsTooltips() {
 
       <Modal>
         <Tooltip
+          className="custom-tooltip"
           id="magnet2"
           content={
             <Modal.Open opens="contacts">
-              <p>Контакты</p>
+              <Name>Контакты</Name>
             </Modal.Open>
           }
+          arrowColor="#00584d"
+          border="3px solid #00584d"
           clickable
         />
         <Modal.Window name="contacts" width="34">
@@ -40,38 +75,50 @@ function MagnetsTooltips() {
 
       <Modal>
         <Tooltip
+          className="custom-tooltip"
           id="magnet3"
           content={
-            <Modal.Open opens={`about-me`}>
-              <p>Обо мне</p>
+            <Modal.Open opens="about-me">
+              <Name>Обо мне</Name>
             </Modal.Open>
           }
+          globalCloseEvents={["mousedown"]}
+          arrowColor="#00584d"
+          border="3px solid #00584d"
+          ts="click"
           clickable
         />
-        <Modal.Window name={`about-me`} width="34">
-          <MagnetInfo magnet={`magnet3`} />
+        <Modal.Window name="about-me" width="34">
+          <MagnetInfo magnet="magnet3" />
         </Modal.Window>
       </Modal>
 
+      {/* links */}
       <Tooltip
+        className="custom-tooltip"
         id="magnet0"
         content={
-          <a href={linkGithub} target="_blank">
+          <Link href={linkGithub} target="_blank">
             Мой github
-          </a>
+          </Link>
         }
+        arrowColor="#00584d"
+        border="3px solid #00584d"
         clickable
       />
       <Tooltip
+        className="custom-tooltip"
         id="magnet4"
         content={
-          <a href={linkMeme} target="_blank">
+          <Link href={linkMeme} target="_blank">
             ;)
-          </a>
+          </Link>
         }
+        arrowColor="#00584d"
+        border="3px solid #00584d"
         clickable
       />
-    </>
+    </Container>
   );
 }
 
